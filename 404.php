63700913aa10e965b1bebd90a99ea7e894bd03c0
Maksim Rudenko <rudenko.programmer@gmail.com>
2016-03-06 00:04:04 +0200

    404

--- 404.php
+++ 404.php
@@ -8,23 +8,40 @@
  */
 
 get_header(); ?>
-
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'twentyfifteen' ); ?></h1>
-				</header><!-- .page-header -->
-
-				<div class="page-content">
-					<p><?php _e( 'It looks like nothing was found at this location. Maybe try a search?', 'twentyfifteen' ); ?></p>
-
-					<?php get_search_form(); ?>
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
-
+<style>
+	.main-404{
+		background-color: #dfdfe2;
+		display: flex;
+		width: 100%;
+		flex-direction: row;
+	}
+	.main-404 >div {
+		margin: auto;
+	}
+	.main-404 h1 {
+		font-family: 'Condiment', cursive;
+		margin-top: 20px;
+		color: #EA3C4D;
+		font-size: 2.4em;
+		text-transform: capitalize;
+		font-weight: bold;
+	}
+	.text{
+		padding-left: 5%;
+		padding-right: 5%;
+	}
+</style>
+<div class="container">
+		<div class="main-404">
+			<div class="text">
+				<h1>Запрашиваемая страница не найдена! ....</h1>
+				<p>Извините! Очевидно, что документ, который вы искали был перемещен или больше не существуют ...</p>
+			</div>
+			<div class="banner">
+				<img src="<?php echo get_template_directory_uri(); ?>/img/banner.png" alt="" />
+			</div>
+			<div style="clear:both;"></div>
+		</div>
+</div>
+<div class="hidden-sm hidden-xs separator-line"></div>
 <?php get_footer(); ?>
