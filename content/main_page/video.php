f16e1f8f4c7e3ce9d5e0dbc9d9b80eca182d779c
Polomkin A <polomkinanton@gmail.com>
2016-03-30 18:16:42 +0300

    убрал ошибки

--- content/main_page/video.php
+++ content/main_page/video.php
@@ -4,8 +4,6 @@
  * Date: 01.03.2016
  */
 ?>
-<<<<<<< HEAD
-
     <div>
         <div class="col-lg-12 col-md-12">
         <style type="text/css">
@@ -18,7 +16,6 @@
                 <h2 class="main-blocks__title">Видео блог:</h2>
                 <div class="text-center">
                     <iframe width="100%" height="515" src="https://www.youtube.com/embed/tweCOkjNbhQ" frameborder="0" allowfullscreen></iframe>
-=======
 <div class="container">
     <div class="row">
         <div class="col-lg-12 col-md-12">
@@ -26,13 +23,8 @@
                 <h2 class="main-blocks__title">Видео блог:</h2>
                 <div class="text-center">
                     <iframe width="1200" height="515" src="https://www.youtube.com/embed/tweCOkjNbhQ" frameborder="0" allowfullscreen></iframe>
->>>>>>> 4212874c6093d996ea61d6a95fd77e77468a6374
                 </div>
             </div>
         </div>
     </div>
-<<<<<<< HEAD
-
-=======
 </div>
->>>>>>> 4212874c6093d996ea61d6a95fd77e77468a6374
